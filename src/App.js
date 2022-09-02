2a1a318737e3e809afb3bdd90071ffc29b27dbd0
sol <soool1021@gmail.com>
2022-09-02 15:49:59 +0900

    상품 상세 페이지

--- src/App.js
+++ src/App.js
@@ -7,11 +7,19 @@ import ProductPage from "./product";
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact={true} path={"/"} component={MainPageComponent}></Route>
-        <Route exact={true} path={"/product/:id"} component={ProductPage}></Route>
-        <Route exact={true} path={"/upload"} component={UploadPage}></Route>
-      </Switch>
+      <div id="header">
+        <div id="header-area">
+          <img src="/images/icons/logo.png" alt="로고 이미지" />
+        </div>
+      </div>
+      <div id="body">
+        <Switch>
+          <Route exact={true} path={"/"} component={MainPageComponent}></Route>
+          <Route exact={true} path={"/product/:id"} component={ProductPage}></Route>
+          <Route exact={true} path={"/upload"} component={UploadPage}></Route>
+        </Switch>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,7 +1,41 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
-  return <h1>상품 상세 페이지{id} 상품</h1>;
+  const [product, setProduct] = useState(null);
+  useEffect(function () {
+    axios
+      .get(`https://dc568c98-a89e-4afd-ad6d-5f74648daf81.mock.pstmn.io/product/${id}`)
+      .then(function (result) {
+        setProduct(result.data);
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2022년 09월 02일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 export default ProductPage;
